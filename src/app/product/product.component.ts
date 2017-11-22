bf8ebe51e1eb6396e8ca0b165808a9fbf1561c26
beytullah <32388523+beytullahk54@users.noreply.github.com>
2017-11-22 23:01:05 +0300

    temel

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './product.service';
+import {Http , Response} from '@angular/http';
+
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+    providers:[ProductService]
+})
+export class ProductComponent implements OnInit {
+
+  products:Product[];
+
+  constructor(private productService:ProductService) {
+
+  }
+
+  ngOnInit() {
+    this.getProducts()
+  }
+  getProducts(){
+      this.productService.getProducts().subscribe(p=>
+      {
+          this.products=p
+      });
+  }
+
+}
